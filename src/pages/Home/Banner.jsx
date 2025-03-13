44bc5523a0d28c1d6cdaaf446271069c3dd3b1b1
omlan99 <omlan10616@gmail.com>
2025-03-13 10:04:38 +0600

    changed the nav color and removed margin

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -3,26 +3,26 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="h-[500px] flex items-center p-4">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card bg-base-100 w-full max-w-xl shrink-0 shadow-xl">
         <div className="card-body">
           <p>Drive anywhere, anytime with</p>
           <h2 className="text-2xl font-medium">Self-drive car rentals in</h2>
 
           <fieldset className="fieldset">
             <label className="fieldset-label">Location</label>
-            <input type="text" className="input" placeholder="Location" />
-            <div className="flex justify-between ">
-              <div>
+            <input type="text" className="input w-full" placeholder="Location" />
+            <div className="flex gap-5 ">
+              <div className="w-1/2">
                 <label className="fieldset-label">Trip Starts</label>
                 <input type="date" className="input" placeholder="Start Date" />
               </div>
-              <div>
+              <div className="w-1/2">
                 <label className="fieldset-label">Trip Ends</label>
                 <input type="date" className="input" placeholder="Start Date" />
               </div>
             </div>
 
-            <button className="btn bg-[#1149AD] text-white mt-4">Search</button>
+            <button className="btn bg-[#4445fb] text-white mt-4 rounded-xl">Search</button>
           </fieldset>
         </div>
       </div>
