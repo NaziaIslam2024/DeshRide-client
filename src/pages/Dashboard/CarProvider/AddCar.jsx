1009e38684d72f6d77742212a7d562cd5271262b
Md Mostafizur Rahman Chowdhury <mostafiz@Mds-MacBook-Pro.local>
2025-03-22 08:27:53 +0600

    add heading in AddCar-->CarProvider

--- src/pages/Dashboard/CarProvider/AddCar.jsx
+++ src/pages/Dashboard/CarProvider/AddCar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AddCar = () => {
     return (
         <div>
-            
+            <h2>Do you want to add Car? Please provide these information.</h2>
         </div>
     );
 };
