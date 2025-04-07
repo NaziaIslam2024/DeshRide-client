5bf8d4d9e8ba4b7e1a2cb8b2848008f7fad652a1
omlan99 <omlan10616@gmail.com>
2025-04-07 17:46:26 +0600

    fixed conflicts

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
   baseURL: "http://localhost:5001",
+   // baseURL: "https://deshride-server.onrender.com/",
   headers: {
     'Content-Type': 'application/json',
   },
