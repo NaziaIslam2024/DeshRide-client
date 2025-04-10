e9066e20e0cf339ca00c5ad2590d59b3deddb5ed
syedmehedi34 <syedmeehdi34@gmail.com>
2025-04-10 12:13:21 +0600

    added the rent a car page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -15,6 +15,8 @@ import CarSelect from "../pages/Dashboard/CarProvider/CarSelect";
 import AllCars from "../pages/Dashboard/Admin/AllCars";
 import MyAddedCar from "../pages/Dashboard/CarProvider/MyAddedCar";
 import AllListedCars from "../pages/Dashboard/Consumer/AllListedCars";
+import RentACar from "../pages/RentACar/RentACar";
+import CarDetails from "../pages/RentACar/CarDetails";
 
 export const router = createBrowserRouter([
   {
@@ -27,8 +29,16 @@ export const router = createBrowserRouter([
         element: <Home></Home>,
       },
       {
-        path : "/about",
-        element: <About></About>
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/rent-a-car",
+        element: <RentACar></RentACar>,
+      },
+      {
+        path: "/rent-car/:carId",
+        element: <CarDetails></CarDetails>,
       },
       {
         path: "login",
@@ -41,8 +51,8 @@ export const router = createBrowserRouter([
 
       {
         path: "privacy&policy",
-        element: <PrivacyPolicy></PrivacyPolicy>
-      }
+        element: <PrivacyPolicy></PrivacyPolicy>,
+      },
     ],
   },
 
@@ -51,38 +61,38 @@ export const router = createBrowserRouter([
     element: <Dashboard></Dashboard>,
     children: [
       {
-        path: '/Dashboard',
-        element: <MyProfile></MyProfile>
+        path: "/Dashboard",
+        element: <MyProfile></MyProfile>,
       },
       {
-        path: 'vehicle',
-        element: <MyAddedCar></MyAddedCar>
+        path: "vehicle",
+        element: <MyAddedCar></MyAddedCar>,
       },
       {
-        path: 'my-profile',
-        element: <MyProfile></MyProfile>
+        path: "my-profile",
+        element: <MyProfile></MyProfile>,
       },
       {
-        path: 'all-user',
-        element: <AllUsers></AllUsers>
+        path: "all-user",
+        element: <AllUsers></AllUsers>,
       },
       {
-        path: 'add-car',
-        element: <AddCar></AddCar>
+        path: "add-car",
+        element: <AddCar></AddCar>,
       },
       {
-        path: 'all-vehicle',
-        element: <CarSelect></CarSelect>
+        path: "all-vehicle",
+        element: <CarSelect></CarSelect>,
       },
       {
-        path: 'all-cars',
-        element: <AllCars></AllCars>
+        path: "all-cars",
+        element: <AllCars></AllCars>,
       },
       // consumer
       {
-        path: 'all-listed-cars',
-        element: <AllListedCars></AllListedCars>
+        path: "all-listed-cars",
+        element: <AllListedCars></AllListedCars>,
       },
-    ]
+    ],
   },
 ]);
