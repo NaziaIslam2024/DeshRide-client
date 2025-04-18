31af49ff5e8b18e26a1031381bc89f1c8ddada13
syedmehedi34 <syedmeehdi34@gmail.com>
2025-04-18 20:26:35 +0600

    axios changed

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,11 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../providers/AuthProvider";
 
 const axiosSecure = axios.create({
-  baseURL: "https://desh-ride-server.vercel.app",
-  // baseURL: "https://deshride-server.onrender.com/",
-  // baseURL: "https://assetflow-livid.vercel.app",
-
-  // baseURL: "https://deshride-server.onrender.com/",
+  // baseURL: "https://desh-ride-server.vercel.app",
+  baseURL: "http://localhost:5001",
 });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
